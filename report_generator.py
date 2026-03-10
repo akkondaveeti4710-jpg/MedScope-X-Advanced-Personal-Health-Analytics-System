e7f7ab6b5967ac160edf0ef57955b62abd6d7ee7
akkondaveeti4710-jpg <akkondaveeti4710@markcubanai.org>
2026-03-10 13:29:38 -0500

    Create report_generator.py

---
+++ report_generator.py
@@ -0,0 +1,17 @@
+def generate_report(name,bmi,bmr,hydration,cardio,diabetes,health):
+
+    print("\n====== MEDSCOPE X REPORT ======\n")
+
+    print("Patient:",name)
+
+    print("BMI:",bmi)
+
+    print("BMR:",bmr)
+
+    print("Hydration Score:",hydration)
+
+    print("Cardiovascular Risk:",cardio)
+
+    print("Diabetes Risk:",diabetes)
+
+    print("\nOverall Health Score:",health,"/100")
