14f1f2b351fa05a512e33ddd9ab375d5f08cb206
akkondaveeti4710-jpg <akkondaveeti4710@markcubanai.org>
2026-03-10 13:26:55 -0500

    Create bmi_module.py

---
+++ bmi_module.py
@@ -0,0 +1,22 @@
+def calculate_bmi(weight,height):
+
+    height_m = height / 100
+
+    bmi = weight / (height_m ** 2)
+
+    return round(bmi,2)
+
+
+def bmi_category(bmi):
+
+    if bmi < 18.5:
+        return "Underweight"
+
+    elif bmi < 25:
+        return "Normal"
+
+    elif bmi < 30:
+        return "Overweight"
+
+    else:
+        return "Obese"
